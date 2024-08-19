12201ca78dcd7285465fa3875215b257b8ae8c77
Salihin <salsatsat@gmail.com>
2024-08-19 16:01:52 +0800

    feat(main): Create simple program
    Program reads port from .env file, creates a server and api.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"datahandler_go/helpers"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+)
+
+func main() {
+	port := helpers.EnvVariable("PORT")
+
+	app := fiber.New()
+
+	// Use middlewares
+	app.Use(requestid.New())
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Create a map to hold the JSON data with a status code
+		response := map[string]interface{}{
+			"message":     "Hello, World!",
+			"status_code": fiber.StatusOK, // Use Fiber's built-in status code
+		}
+		// Send the JSON response
+		return c.JSON(response)
+	})
+
+	fmt.Printf("Server listening on port %s\n", port)
+	log.Fatal(app.Listen(":" + port))
+}
