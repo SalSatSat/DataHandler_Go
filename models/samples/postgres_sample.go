87f0ef1a4d21e960f36e2b3183a56ec492a11e94
Salihin <salsatsat@gmail.com>
2024-09-23 11:30:59 +0800

    feat(models): Add "label" field to postgres_sample model

--- models/samples/postgres_sample.go
+++ models/samples/postgres_sample.go
@@ -4,5 +4,6 @@ import "gorm.io/gorm"
 
 type Postgres_Sample struct {
 	gorm.Model
+	Label string  `json:"label" gorm:"type:varchar(255);not null"`
 	Value float64 `json:"value" gorm:"type:numeric;not null"`
 }
