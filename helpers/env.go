f94077da3401f9c4e7ea6b56265fe96399f0f239
Salihin <salsatsat@gmail.com>
2024-09-25 13:38:02 +0800

    style(helpers): Start comments with uppercase letter

--- helpers/env.go
+++ helpers/env.go
@@ -8,11 +8,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// use godot package to load/read the .env file and
-// return the value of the key
+// Use godot package to load/read the .env file and
+// Return the value of the key
 func EnvVariable(key string) string {
 
-	// load .env file
+	// Load .env file
 	err := godotenv.Load(".env")
 
 	if err != nil {
